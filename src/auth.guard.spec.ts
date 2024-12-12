1d42733a5bb2db4aeef064fdd0fac56f9ac157d8
shadeisroot <dschristensen@proton.me>
2024-12-12 10:22:27 +0100

    auth service removed

--- src/auth.guard.spec.ts
+++ src/auth.guard.spec.ts
@@ -1,30 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-import { AuthService } from './Services/auth.service';
 import { Router } from '@angular/router';
 
 describe('AuthGuard', () => {
-  let authServiceMock: Partial<AuthService>;
-  let routerMock: Partial<Router>;
   let authGuardInstance: AuthGuard;
 
   beforeEach(() => {
-    authServiceMock = {
-    };
-
-    routerMock = {
-      navigate: jasmine.createSpy('navigate')
-    };
-
-    TestBed.configureTestingModule({
-      providers: [
-        { provide: AuthService, useValue: authServiceMock },
-        { provide: Router, useValue: routerMock }
-      ]
-    });
-
-    authGuardInstance = new AuthGuard(TestBed.inject(AuthService), TestBed.inject(Router));
   });
 
   const executeGuard: CanActivateFn = (...guardParameters) =>
